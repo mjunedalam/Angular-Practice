03de8e4b152cee412d6d396a053eddf8a1b9fdd6
juned.alam <juned.alam@graviteesource.com>
2026-02-26 16:18:55 +0530

    fix: design

--- src/app/features/wellbore/presentation/presentation.component.ts
+++ src/app/features/wellbore/presentation/presentation.component.ts
@@ -1,24 +1,31 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnInit,
-  inject,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
 
 import { WellStore } from '../../../core/store/well.store';
 import { WellNameChipsComponent } from '../well-name-chips/well-name-chips.component';
 import { DepthScaleComponent } from '../depth-scale/depth-scale.component';
 import { WellBoreViewComponent } from '../well-bore-view/well-bore-view.component';
+import { MiscPresWellDataComponent } from '../misc-pres-well-data/misc-pres-well-data.component';
+import { PickedFormationTopsComponent } from '../picked-formation-tops/picked-formation-tops.component';
+import { OffsetWwellsComponent } from '../offset-wwells/offset-wwells.component';
+import { ActiveWwellMapComponent } from '../active-wwell-map/active-wwell-map.component';
 
 @Component({
-  selector: 'app-presentation',
+  selector: 'app-persentation',
   standalone: true,
-  imports: [WellNameChipsComponent, DepthScaleComponent, WellBoreViewComponent],
+  imports: [
+    WellNameChipsComponent, 
+    DepthScaleComponent, 
+    WellBoreViewComponent,
+    MiscPresWellDataComponent,
+    PickedFormationTopsComponent,
+    OffsetWwellsComponent,
+    ActiveWwellMapComponent
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './presentation.component.html',
   styleUrl: './presentation.component.scss',
 })
-export class PresentationComponent implements OnInit {
+export class PersentationComponent implements OnInit {
   protected readonly store = inject(WellStore);
 
   ngOnInit(): void {
