2965f6d7d18ae02ad645799d31fca0408b96df2e
mjunedalam <juned.alam.dev@gmail.com>
2026-03-02 13:45:29 +0300

    Update misc-pres-well-data.component.ts

--- src/app/features/wellbore/misc-pres-well-data/misc-pres-well-data.component.ts
+++ src/app/features/wellbore/misc-pres-well-data/misc-pres-well-data.component.ts
@@ -1,15 +1,32 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { MiscWellData } from '../../../core/store/well.store';
 import { DecimalPipe, NgIf } from '@angular/common';
+
+import { MiscWellData } from '../../../core/stores/wwell-data/well.store';
 
 @Component({
   selector: 'app-misc-pres-well-data',
   standalone: true,
- imports: [NgIf, DecimalPipe],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgIf, DecimalPipe],
   templateUrl: './misc-pres-well-data.component.html',
-  styleUrl: './misc-pres-well-data.component.scss'
+  styleUrl: './misc-pres-well-data.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush // Optimized performance
 })
 export class MiscPresWellDataComponent {
+
   readonly data = input<MiscWellData | null>(null);
+
+
+  // private readonly store = inject(WellStore);
+
+
+  // readonly misc = this.store.miscWellData;
+
+
+  // constructor() {
+
+
+
+  //   this.store.loadWellNames();
+  // }
+
 }
