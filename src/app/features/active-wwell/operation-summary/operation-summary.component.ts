27de73049d571e5bbcf9d8dcf1217e1a3fc3e2bf
mjunedalam <juned.alam.dev@gmail.com>
2026-05-19 11:05:49 +0300

    Update operation-summary.component.ts

--- src/app/features/active-wwell/operation-summary/operation-summary.component.ts
+++ src/app/features/active-wwell/operation-summary/operation-summary.component.ts
@@ -1,10 +1,12 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActiveWwellFormService } from '../active-wwell-form.service';
 import { ActiveWwellStore } from '../store/active-wwell.store';
 
 @Component({
   selector: 'app-operation-summary',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './operation-summary.component.html',
   styleUrl: './operation-summary.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,4 +15,11 @@ export class OperationSummaryComponent {
   private readonly store = inject(ActiveWwellStore);
 
   protected readonly data = this.store.operationSummary;
+
+  formService = inject(ActiveWwellFormService);
+  form: any = this.formService.drillingRemarksForm;
+
+  ngOnInit() {
+
+  }
 }
