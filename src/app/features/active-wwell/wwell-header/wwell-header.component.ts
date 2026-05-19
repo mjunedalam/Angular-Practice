5decd96baf29536556f705e6d802feee0469668c
mjunedalam <juned.alam.dev@gmail.com>
2026-05-19 11:10:19 +0300

    Update wwell-header.component.ts

--- src/app/features/active-wwell/wwell-header/wwell-header.component.ts
+++ src/app/features/active-wwell/wwell-header/wwell-header.component.ts
@@ -1,24 +1,22 @@
 import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { ActiveWwellStore } from '../store/active-wwell.store';
-import { ActiveWwellUiStore } from '../active-wwell-ui.store';
-import { deriveStatusLabel } from '../active-wwell.helpers';
+import { ActiveWwellFormService } from '../active-wwell-form.service';
 
 @Component({
   selector: 'app-wwell-header',
   standalone: true,
   imports: [],
   templateUrl: './wwell-header.component.html',
-  styleUrl: './wwell-header.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './wwell-header.component.scss'
 })
 export class WwellHeaderComponent {
-  private readonly store = inject(ActiveWwellStore);
-  private readonly uiStore = inject(ActiveWwellUiStore);
+  protected readonly store = inject(ActiveWwellStore);
+  protected readonly activeWwellFormService = inject(ActiveWwellFormService);
 
   protected readonly data = this.store.wellHeaderData;
 
-  protected readonly currentStatus = computed(() => {
-    const epANum = this.store.selectedEpANum();
-    return this.uiStore.statusForWell(epANum) ?? deriveStatusLabel(this.store.selectedWell());
-  });
+  protected readonly currentStatus = this.store.status();
+
+  protected readonly drillingForm = this.activeWwellFormService.drillingRemarksForm
+  
 }
