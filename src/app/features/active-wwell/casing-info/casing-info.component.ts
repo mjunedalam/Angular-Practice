a946ff7da6cfc2fdcdc0ced97a1eefc015ebae9a
mjunedalam <juned.alam.dev@gmail.com>
2026-05-19 11:03:05 +0300

    Update casing-info.component.ts

--- src/app/features/active-wwell/casing-info/casing-info.component.ts
+++ src/app/features/active-wwell/casing-info/casing-info.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+
+import { ColDef, themeQuartz } from 'ag-grid-community';
+import { AgGridAngular } from 'ag-grid-angular';
 import { ActiveWwellStore } from '../store/active-wwell.store';
 
 @Component({
   selector: 'app-casing-info',
   standalone: true,
-  imports: [],
+  imports: [AgGridAngular],
   templateUrl: './casing-info.component.html',
   styleUrl: './casing-info.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -12,5 +15,46 @@ import { ActiveWwellStore } from '../store/active-wwell.store';
 export class CasingInfoComponent {
   private readonly store = inject(ActiveWwellStore);
 
-  protected readonly data = this.store.casingInfo;
+  protected readonly data = this.store.allCasingData;
+
+  columnDefs: ColDef[] = [
+    { headerName: 'Casing Type', field: 'csgType' },
+    { headerName: 'Casing Size', field: 'csgSize' },
+    { headerName: 'Casing Depth', field: 'csgDepth' },
+    { headerName: 'Casing Bot Depth', field: 'csgBotDpth' }
+  ];
+
+  readonly theme = themeQuartz.withParams({
+    accentColor: '#2563eb',
+    backgroundColor: '#ffffff',
+    foregroundColor: '#1e3a8a',
+    headerBackgroundColor: '#2563eb',
+    headerTextColor: '#ffffff',
+    headerFontSize: 13,
+    headerFontWeight: 700,
+    rowHoverColor: '#eff6ff',
+    oddRowBackgroundColor: '#eff6ff',
+    borderColor: 'transparent',
+    borderRadius: 0,
+    fontSize: 12,
+    rowHeight: 36,
+    headerHeight: 40,
+    spacing: 2,
+    fontFamily: 'inherit',
+    cellHorizontalPaddingScale: 1.5,
+  });
+
+
+  defaultColDef: ColDef = {
+    sortable: true,
+    filter: false,
+    resizable: true,
+    minWidth: 130,
+    //valueGetter: params => params.value ?? 'N/A'
+  };
+
+  onGridReady(params: any) {
+    params.api.sizeColumnsToFit();
+  }
+
 }
