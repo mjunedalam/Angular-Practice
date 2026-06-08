4c1534aaf4768e77aa9c7c102a401d903503c755
juned alam <juned.alam@graviteesource.com>
2026-06-08 13:16:50 +0530

    sync

--- src/app/features/active-wwell/casing-info/casing-info.component.ts
+++ src/app/features/active-wwell/casing-info/casing-info.component.ts
@@ -20,7 +20,7 @@ export class CasingInfoComponent {
   columnDefs: ColDef[] = [
     { headerName: 'Casing Type', field: 'csgType' },
     { headerName: 'Casing Size', field: 'csgSize' },
-    { headerName: 'Casing Depth', field: 'csgDepth' }
+    { headerName: 'Casing Bottom Depth', field: 'csgDepth' }
   ];
 
   readonly theme = themeQuartz.withParams({
